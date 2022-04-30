622f3f368317d85fdebbe5fe104de405e39228a4
dishankgoel <dishankgoel1729@gmail.com>
2022-04-30 17:30:14 +0530

    Add run script

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v /sys/kernel/debug:/sys/kernel/debug --privileged hyperion
+docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v /sys/kernel/debug:/sys/kernel/debug --name hyperion --privileged hyperion
